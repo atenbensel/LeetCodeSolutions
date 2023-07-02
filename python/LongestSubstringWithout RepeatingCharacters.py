0f3ade4b72a6e7c2fcc8ee4834bfcd12f769d75b
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-07-02 15:07:13 +0000

    longest substring solution

--- python/LongestSubstringWithout RepeatingCharacters.py
+++ python/LongestSubstringWithout RepeatingCharacters.py
@@ -2,6 +2,26 @@
 
 Question:
 
+
+
 Solution:
 
 '''
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        if not s:
+            return 0
+
+        chars = {s[0]}
+        longest_length = 1
+        l, r = 0, 0
+
+        while r < len(s) - 1:
+            while s[r + 1] in chars:
+                chars.remove(s[l])
+                l += 1
+            r += 1
+            chars.add(s[r])
+            longest_length = max(longest_length, r - l + 1)
+        return longest_length
