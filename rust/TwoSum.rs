5f845a543feae3441e00a48d06d29a2aaff581d5
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-07-02 15:19:11 +0000

    rust two sum solution

--- rust/TwoSum.rs
+++ rust/TwoSum.rs
@@ -2,3 +2,20 @@
 /// You may assume that each input would have exactly one solution, and you may not use the same element twice.
 /// You can return the answer in any order.
 
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    use std::collections::HashMap;
+    let mut r: HashMap<i32, i32> = HashMap::new();
+    for i in 0..nums.len() {
+        r.insert(nums[i], i as i32);
+    }
+
+    for i in 0..nums.len() {
+        match r.get(&(target - nums[i])) {
+            Some(index) if i as i32 != *index => return vec![i as i32, index.clone()],
+            None => {}
+            _ => {}
+        }
+    }
+    vec![]
+}
+}
