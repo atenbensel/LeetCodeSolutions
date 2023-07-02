9e5ff45a02c4a3b7053c160ae176cb2f3253470d
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-07-02 15:54:55 +0000

    juleetcode day 2 solution

--- python/1601_MaximumNumberofAchievableTransferRequests.py
+++ python/1601_MaximumNumberofAchievableTransferRequests.py
@@ -5,4 +5,29 @@
 # All buildings are full, so a list of requests is achievable only if for each building, the net change in employee transfers is zero. This means the number of employees leaving is equal to the number of employees moving in. For example if n = 3 and two employees are leaving building 0, one is leaving building 1, and one is leaving building 2, there should be two employees moving to building 0, one employee moving to building 1, and one employee moving to building 2.
 # Return the maximum number of achievable requests.
 
+# Solution:
+
+class Solution(object):
+    def maximumRequests(self, n, requests):
+        self.max_requests = 0
+
+        def backtrack(index, curr_requests, net_changes):
+            if index == len(requests):
+                if all(change == 0 for change in net_changes):
+                    self.max_requests = max(self.max_requests, curr_requests)
+                return
+
+            fromi, toi = requests[index]
+            net_changes[fromi] -= 1
+            net_changes[toi] += 1
+            backtrack(index + 1, curr_requests + 1, net_changes)
+
+            net_changes[fromi] += 1
+            net_changes[toi] -= 1
+            backtrack(index + 1, curr_requests, net_changes)
+
+        net_changes = [0] * n
+        backtrack(0, 0, net_changes)
+
+        return self.max_requests
 
