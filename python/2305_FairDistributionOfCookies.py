64528a50bff178e1699eca96a617ffe6ca9029fd
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-07-03 13:49:12 +0000

    2305 solution

--- python/2305_FairDistributionOfCookies.py
+++ python/2305_FairDistributionOfCookies.py
@@ -2,4 +2,20 @@
 # The unfairness of a distribution is defined as the maximum total cookies obtained by a single child in the distribution.
 # Return the minimum unfairness of all distributions.
 
-#Solution:
+# Solution:
+
+class Solution(object):
+    def distributeCookies(self, cookies, k):
+        
+        total = [0]*(1<<len(cookies))
+        for mask in xrange(1<<len(cookies)):
+            total[mask] = sum(cookies[i] for i in xrange(len(cookies)) if mask&(1<<i))
+        dp = [[float("inf")]*(1<<len(cookies)) for _ in xrange(2)]
+        dp[0][0] = 0
+        for i in xrange(k):
+            for mask in xrange(1<<len(cookies)):
+                submask = mask
+                while submask:
+                    dp[(i+1)%2][mask] = min(dp[(i+1)%2][mask], max(total[submask], dp[i%2][mask^submask]))
+                    submask = (submask-1)&mask
+        return dp[k%2][-1]
