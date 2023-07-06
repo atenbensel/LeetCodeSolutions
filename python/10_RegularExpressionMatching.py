2af9200931928b59316f1dabf5d2c0fe60934804
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-07-06 13:32:03 -0400

    Create 10_RegularExpressionMatching.py

---
+++ python/10_RegularExpressionMatching.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        dp = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
+        
+        dp[0][0] = True
+        
+        for j in range(2, len(p) + 1):
+            if p[j - 1] == '*':
+                dp[0][j] = dp[0][j - 2]
+        
+        for i in range(1, len(s) + 1):
+            for j in range(1, len(p) + 1):
+                if s[i - 1] == p[j - 1] or p[j - 1] == '.':
+                    dp[i][j] = dp[i - 1][j - 1]
+                elif p[j - 1] == '*':
+                    dp[i][j] = dp[i][j - 2]
+                    if s[i - 1] == p[j - 2] or p[j - 2] == '.':
+                        dp[i][j] = dp[i][j] or dp[i - 1][j]
+                else:
+                    dp[i][j] = False
+        
+        return dp[-1][-1]
