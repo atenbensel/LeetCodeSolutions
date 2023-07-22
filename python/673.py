61fc2daaf8ad61ed36ef1adab255b9e43a0525ca
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-07-21 19:21:54 -0500

    Create 673.py

---
+++ python/673.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def findNumberOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        
+        n = len(nums)
+        lengths = [1] * n
+        counts = [1] * n
+
+        for i in range(n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    if lengths[j] + 1 > lengths[i]:
+                        lengths[i] = lengths[j] + 1
+                        counts[i] = counts[j]
+                    elif lengths[j] + 1 == lengths[i]:
+                        counts[i] += counts[j]
+        
+        max_length = max(lengths)
+        return sum(counts[i] for i in range(n) if lengths[i] == max_length)
