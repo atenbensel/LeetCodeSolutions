6b3749a6eab803c187e2094b2736231c10761491
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-08-01 14:09:42 -0400

    Create 77.py

---
+++ python/77.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        def backtrack(start, path):
+            if len(path) == k:
+                combinations.append(path[:])
+                return
+            
+            for i in range(start, n + 1):
+                path.append(i)
+                backtrack(i + 1, path)
+                path.pop()
+        
+        combinations = []
+        backtrack(1, [])
+        return combinations
