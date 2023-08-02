3472608c986ca0ed1b428d569d9d5046f6ec4688
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-08-02 10:43:37 -0400

    Create 46.py

---
+++ python/46.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        def backtrack(start):
+            if start == len(nums):
+                permutations.append(nums[:])
+                return
+            
+            for i in range(start, len(nums)):
+                nums[start], nums[i] = nums[i], nums[start]
+                backtrack(start + 1)
+                nums[start], nums[i] = nums[i], nums[start]
+        
+        permutations = []
+        backtrack(0)
+        return permutations
