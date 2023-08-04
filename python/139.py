2fde75d9a76834baa58ec05d4edf61b9b966e5b6
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-08-04 12:38:42 -0400

    Create 139.py

---
+++ python/139.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        n = len(s)
+        dp = [False] * (n + 1)
+        dp[0] = True
+        
+        wordSet = set(wordDict)
+        
+        for i in range(1, n + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordSet:
+                    dp[i] = True
+                    break
+        
+        return dp[n]
