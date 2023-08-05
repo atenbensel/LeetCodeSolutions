01e1518569c41c94c303e5e572f11f000b8b1957
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-08-05 02:24:51 -0400

    Create 28.py

---
+++ python/28.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if needle == "":
+            return 0
+        
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        
+        return -1
