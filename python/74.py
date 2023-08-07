276ac3b896bb3be77b23b77807bd75dad8f2bff0
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-08-07 11:41:55 -0400

    Create 74.py

---
+++ python/74.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        m, n = len(matrix), len(matrix[0])
+        left, right = 0, m * n - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            mid_value = matrix[mid // n][mid % n]
+
+            if mid_value == target:
+                return True
+            elif mid_value < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return False
+        
