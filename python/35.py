90965fb60d46d97be18d65c56f3140811de1a065
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-08-25 11:56:17 -0400

    Create 35.py

---
+++ python/35.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid +1
+            else:
+                right = mid - 1
+
+        return left
