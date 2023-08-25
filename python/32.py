80f3dfd0c9a7b5710edf34b94ec95fc0bbaefe6e
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-08-25 11:13:49 -0400

    Create 32.py

---
+++ python/32.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        dp = [0] * n
+        max_length = 0
+
+        for i in range(1, n):
+            if s[i] == ')':
+                if s[i - 1] == '(':
+                    dp[i] = (dp[i - 2] if i >= 2 else 0) + 2
+                elif i - dp[i - 1] > 0 and s[i - dp[i - 1] - 1] == '(':
+                    dp[i] = dp[i - 1] + (dp[i - dp[i - 1] - 2] if i - dp[i - 1] >= 2 else 0) + 2
+                max_length = max(max_length, dp[i])
+
+        return max_length
