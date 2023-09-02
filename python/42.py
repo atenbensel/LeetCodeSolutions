e76de09a837a736cc28e228f7f50b9e9a106d90b
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-09-02 17:55:00 -0400

    Create 42.py

---
+++ python/42.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        if not height:
+            return 0
+
+        left, right = 0, len(height) - 1
+        left_max, right_max = 0, 0
+
+        total_water = 0
+
+        while left < right:
+            if height[left] < height[right]:
+                if height[left] >= left_max:
+                    left_max = height[left]
+                else:
+                    total_water += left_max - height[left]
+                left += 1
+            else:
+                if height[right] >= right_max:
+                    right_max = height[right]
+                else:
+                    total_water += right_max - height[right]
+                right -= 1
+
+        return total_water
