eccf6855975c7be5dc209c42b7ca92b005e65258
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-09-04 12:54:59 -0400

    Create 48.py

---
+++ python/48.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        for i in range(n):
+            matrix[i] = matrix[i][::-1]
