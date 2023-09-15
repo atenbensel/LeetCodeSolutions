8e560d8964cea1513d5f5ced091ebbe0c8dc6ae8
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-09-15 15:12:31 -0400

    Create 62.py

---
+++ python/62.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [[1] * n for _ in range(m)]
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        
+        return dp[m - 1][n - 1]
