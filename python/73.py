504b820db3b44d995f163776ef99b4a581de0070
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-10-10 10:24:19 -0400

    Create 73.py

---
+++ python/73.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        m, n = len(matrix), len(matrix[0])
+        first_row_has_zero = any(matrix[0][j] == 0 for j in range(n))
+        first_col_has_zero = any(matrix[i][0] == 0 for i in range(m))
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+
+        if first_row_has_zero:
+            for j in range(n):
+                matrix[0][j] = 0
+
+        if first_col_has_zero:
+            for i in range(m):
+                matrix[i][0] = 0
