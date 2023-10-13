6577a5fdc1400ce7370c1d0f45a9cb861962469a
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-10-13 14:17:52 -0400

    Create 75.py

---
+++ python/75.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        left, current, right = 0, 0, len(nums) - 1
+
+        while current <= right:
+            if nums[current] == 0:
+                nums[left], nums[current] = nums[current], nums[left]
+                left += 1
+                current += 1
+            elif nums[current] == 2:
+                nums[current], nums[right] = nums[right], nums[current]
+                right -= 1
+            else:
+                current += 1
