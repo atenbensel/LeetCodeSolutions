bae4ee220b0743f76f6bfda3c8f1e483b51ac481
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-10-14 12:28:28 -0400

    Create 80.py

---
+++ python/80.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+
+        k = 1
+        count =1
+
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                count += 1
+            else:
+                count = 1
+
+            if count <= 2:
+                nums[k] = nums[i]
+                k += 1
+
+        return k
