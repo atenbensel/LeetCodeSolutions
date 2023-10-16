28e052ebecac33b371fe2e9bb0ef38ea97975f01
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-10-16 14:32:12 -0400

    Create 86.py

---
+++ python/86.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def partition(self, head, x):
+        """
+        :type head: ListNode
+        :type x: int
+        :rtype: ListNode
+        """
+        less_head = ListNode(0)
+        greater_head = ListNode(0)
+        less = less_head
+        greater = greater_head
+
+        while head:
+            if head.val < x:
+                less.next = head
+                less = less.next
+            else:
+                greater.next = head
+                greater = greater.next
+            head = head.next
+
+        less.next =greater_head.next
+        greater.next = None
+
+        return less_head.next
