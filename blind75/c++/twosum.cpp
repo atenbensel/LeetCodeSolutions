fbabfdfba529af9fefc4ed46a620c8a4b09719d3
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2024-02-02 18:36:06 -0500

    Update twosum.cpp

---
+++ blind75/c++/twosum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numMap;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (numMap.find(complement) != numMap.end()) {
+                return {numMap[complement], i};
+            }
+            numMap[nums[i]]= i;
+        }
+        return {};
+    }
+};
