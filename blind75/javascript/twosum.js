7d24c2b4099f2c468ec084b5f1ed0c1a5c7dbdbe
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2024-02-02 18:37:33 -0500

    added content

---
+++ blind75/javascript/twosum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+function twoSum(nums, target) {
+    const numMap = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (numMap.has(complement)) {
+            return [numMap.get(complement), i];
+        }
+        numMap.set(nums[i], i);
+    }
+    return [];
+}
