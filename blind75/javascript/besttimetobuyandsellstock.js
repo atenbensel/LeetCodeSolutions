97e0c2095ddfcf01551863ab4a80472e9aab0623
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2024-02-03 22:45:54 -0500

    Create besttimetobuyandsellstock.js

---
+++ blind75/javascript/besttimetobuyandsellstock.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    if (!prices.length) {
+        return 0;
+    }
+
+    let maxProfit = 0;
+    let minPrice = prices[0];
+
+    for (let i = 1; i < prices.length; i++) {
+        maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        minPrice = Math.min(minPrice, prices[i]);
+    }
+
+    return maxProfit;
+};
