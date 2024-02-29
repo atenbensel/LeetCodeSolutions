ba7e2a7ffa61189759fbfb8c8400052755039192
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2024-02-29 18:27:04 -0500

    Create productofarrayexceptself.py

---
+++ blind75/python/productofarrayexceptself.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n =len(nums)
+        result = [1] * n
+
+        left_product = 1
+        for i in range(n):
+            result[i] *= left_product
+            left_product *= nums[i]
+
+        right_product = 1
+        for i in range(n - 1, -1, -1):
+            result[i] *= right_product
+            right_product *= nums[i]
+
+        return result
