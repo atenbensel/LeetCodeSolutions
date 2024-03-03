36b2257ed0d9d1379afabf11cb46154d5aa01aeb
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2024-03-02 22:41:27 -0600

    Create findminimuminrotatedsortedarray.py

---
+++ blind75/python/findminimuminrotatedsortedarray.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+
+        return nums[left]
